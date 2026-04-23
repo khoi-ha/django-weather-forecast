9ae489297df105fd4fee5c79b67db1242db95036
Khoi Ha <williamha186@gmail.com>
2026-04-24 02:52:45 +0300

    Implemented background rotations (fixes #4)

--- forecast/forecastAPI/forecast_api.py
+++ forecast/forecastAPI/forecast_api.py
@@ -7,6 +7,9 @@ import datetime
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+# This path is for the purpose of testing
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+
 # Set up Django environment
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DjangoWeatherForecast.settings")
 django.setup()
@@ -16,7 +19,7 @@ from forecast.openWeather.data_processing import calculate_daily_forecasts
 from forecast.models import Country, City, Weather, InfoType, Background
 
 
-def get_weather_icon(weather_type):
+def get_weather_icon(weather_type) -> str:
     """Get the weather icon for a given weather type.
 
     Args:
@@ -29,11 +32,35 @@ def get_weather_icon(weather_type):
     
     if weather_icon is None:
         print(f"Could not find icon for weather type: {weather_type}.")
-        return None
+        return ""
     return weather_icon.icon
 
+def get_weather_backgrounds(forecasts) -> list[str]:
+    """Get the background images for a given list of forecasts.
 
-def generate_forecast_data(days, city_name, country_name):
+    Args:
+        forecasts (list): The list of weather forecasts to get the backgrounds for.
+
+    Returns:
+        list: A list containing the background images for each weather type.
+    """
+    weathers = [forecast["weather_type"] for forecast in forecasts]
+    unique_weathers = set(weathers)
+    weather_counts = {weather: weathers.count(weather) for weather in unique_weathers}
+    bg_lists = {weather: Background().get_random_backgrounds(weather, count) 
+                for weather, count in weather_counts.items()}
+    
+    bgs = []
+
+    for forecast in forecasts:
+        weather_type = forecast["weather_type"]
+        if bg_lists[weather_type]:
+            bgs.append(bg_lists[weather_type].pop())
+
+    return bgs
+
+
+def generate_forecast_data(days, city_name, country_name) -> dict:
     """Generate weather forecast API data for a specific city and country.
 
     Args:
@@ -47,16 +74,17 @@ def generate_forecast_data(days, city_name, country_name):
     country_code = Country().get_country_code(country_name)
     if country_code is None:
         print(f"Could not find country code for the country: {country_name}.")
-        return
+        return {}
     city_coordinates = City().get_coordinates(city_name, country_code)
     
     if city_coordinates is None:
         print(f"Could not find coordinates for the city: {city_name}.")
-        return
+        return {}
 
     weather_data = get_weather_data(city_coordinates[0], city_coordinates[1])
     daily_forecasts = calculate_daily_forecasts(weather_data, days)
-    
+    backgrounds = get_weather_backgrounds(daily_forecasts)
+
     api_response_entries = []
     for forecast in daily_forecasts:
         entry = {
@@ -64,7 +92,7 @@ def generate_forecast_data(days, city_name, country_name):
             "weather": {
                 "type": forecast["weather_type"],
                 "icon": get_weather_icon(forecast["weather_type"]),
-                "background": Background().get_random_background(forecast["weather_type"]),
+                "background": backgrounds.pop(0) if backgrounds else "",
                 "description": forecast["description"],
                 "temp_min": forecast["temp_min"],
                 "temp_max": forecast["temp_max"],
@@ -86,3 +114,16 @@ def generate_forecast_data(days, city_name, country_name):
     }
 
     return api_response
+
+if __name__ == "__main__":
+
+    # Data to test background retrieval    
+    test_weather_type = "Clouds"
+    test_days_normal = 2
+    test_days_excess = 10
+
+    # Testing background retrieval
+    print(Background().get_random_backgrounds(test_weather_type, 
+                                              test_days_normal))
+    print(Background().get_random_backgrounds(test_weather_type, 
+                                              test_days_excess))
--- forecast/models.py
+++ forecast/models.py
@@ -76,20 +76,33 @@ class Background(models.Model):
     name = models.CharField(max_length=80, unique=True, default="")
     link = models.CharField(max_length=200, unique=True)
 
-    def get_random_background(self, weather_type):
+    def get_random_backgrounds(self, weather_type, days):
         """Get a random background image link for a given weather type.
-
+    
         Args:
             weather_type (str): The type of weather to get the background for.
+            days (int): The number of days to get backgrounds for.
 
         Returns:
-            str: The URL of the background image, or None if not found.
+            list: A list of background image URLs, or an empty list if not found.
         """
-        background = Background.objects.filter(weather_type__name__iexact=weather_type).order_by("?").first()
-        if background:
-            return background.link
-        return ""
-    
-    def __str__(self):        
+
+        link_objs = list(Background.objects
+                         .filter(weather_type__name__iexact=weather_type)
+                         .values_list("link").order_by('?')[:days])
+        links = [k[0] for k in link_objs if k]
+
+        # Repeat means how many time to repeat the link list in the final list
+        repeats = days // len(links)
+        # Padding means the number of items that are taken from the start of the
+        # link list and added to the back of the padded list for it reach the 
+        # required number of days
+        padding = days % len(links)
+        padded_links = links*repeats
+        padded_links.extend(links[:padding])
+
+        return padded_links
+
+    def __str__(self):
         return f"{self.name}"
 
